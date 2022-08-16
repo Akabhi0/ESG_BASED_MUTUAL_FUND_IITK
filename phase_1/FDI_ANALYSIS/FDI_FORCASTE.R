3bc885b65a345230541b2deb323e18a626fc354d
abhishek abhi <artisticdeveloperr@gmail.com>
2022-08-16 19:23:08 +0530

    Phase_1 : Data collection and forecasting based on time series analysis

---
+++ phase_1/FDI_ANALYSIS/FDI_FORCASTE.R
@@ -0,0 +1,58 @@
+#forecasting the data for FDI 
+# 1-visualise the time series
+# 2-stationarize the series
+# 3-Plot ACF/PACF chart find optimal parameter
+# 4-Build the ARIMA model
+# 5-Make predictions
+library(forecast)
+library(tseries)
+library(readxl)
+
+#Import Sales  Dataset
+dataExcel <- read_excel('/Users/akabhi/Desktop/IIT Kanpur/GDP_ANALYSIS_DATA.xlsx')
+
+#Convert sales_k column to Time Series object - 1971 to 1984
+FDI_ts <- ts(dataExcel$`FDI ( Billions of US $)`,start=c(2000),frequency=1)
+
+#Plot Sales Time series using autoplot (forecast library)
+autoplot(FDI_ts)
+
+#Stationarity : A stationary process has a mean and variance 
+#               that do not change overtime and the process does not have trend.
+
+#Perform ADF Test
+#Null Hypothesis - Non Stationary 
+#               (Do NOT Reject if P value > sig lvl (1%, 5%, 10%) / 0.05)
+adfTest(FDI_ts, lag= 1)
+
+#Null Hypothesis not Rejected - Series is Non Stationary
+
+#We will use first order difference to make it Stationary
+FDI_ts_d1 <- diff(FDI_ts, differences = 2)
+adfTest(FDI_ts_d1, lag= 1)
+
+#Check plot
+autoplot(FDI_ts_d1)
+
+#Since P is very small and less than sig lvl - 
+#                 we accept alternate hypothesis
+#ARIMA (p,d,q)
+
+#Run PACF test to select AR term or the p term - correlation between lags
+Pacf(FDI_ts_d1) #choosing 1
+
+#Run ACF test to select MA term or the q term - 
+Acf(FDI_ts_d1) #choosing 1
+
+#BASIC ARIMA - does not work that good
+tsMod <- Arima(y = FDI_ts,order = c(1,2,1))
+
+#Summary of the model
+tsMod
+
+#Forecast 12 periods ahead (1985)
+forecast(tsMod,h=5)
+
+#Plot Sales with forecast 
+autoplot(forecast(tsMod,h=12))
+
